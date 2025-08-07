2b09fe87217e6e0254f033eeea27740e83f55365
Jurgen <94113007+jshaba17@users.noreply.github.com>
2025-08-07 11:01:25 +0200

    Updated

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,6 @@ import HtmlTestRunner
 # Discover tests in the "tests" folder and run them
 suite = unittest.defaultTestLoader.discover('tests', pattern='test_*.py')
 
-# Run the tests and generate an HTML report
+# Run the  tests and generate an HTML report
 runner = HtmlTestRunner.HTMLTestRunner(output='reports')
 runner.run(suite)
